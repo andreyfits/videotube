948e63038b0145daf8a226b8e00f4ea69a1f022a
Andrey Fits <andreyfits@gmail.com>
2020-05-01 20:55:10 +0300

    Creating the column element

--- index.php
+++ index.php
@@ -1,49 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>VideoTube</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="assets/js/commonActions.js"></script>
-</head>
-<body>
-	<div id="pageContainer">
-		<div id="mastHeadContainer">
-			<button class="navShowHide">
-                <img src="assets/images/icons/menu.png" alt="Menu">
-            </button>
-            <a class="logoContainer" href="index.php">
-                <img src="assets/images/icons/VideoTubeLogo.png" alt="Site logo" title="Logo">
-            </a>
-            <div class="searchBarContainer">
-                <form action="search.php" method="get">
-                    <input type="text" class="searchBar" name="term" placeholder="Search...">
-                    <button class="searchButton">
-                        <img src="assets/images/icons/search.png" alt="Search button">
-                    </button>
-                </form>
-            </div>
-            <div class="rightIcons">
-                <a href="upload.php">
-                    <img src="assets/images/icons/upload.png" alt="Upload">
-                </a>
-                <a href="#">
-                    <img src="assets/images/profilePictures/default.png" alt="Profile">
-                </a>
-            </div>
-		</div>
-		<div id="sideNavContainer" style="display: none;">
+<?php require_once ("includes/header.php");?>
 
-		</div>
-		<div id="mainSectionContainer">
-            <div id="mainContentContainer">
-                hello everyone
-            </div>
-		</div>
-	</div>
-</body>
-</html>
+hello everyone
+
+<?php require_once ("includes/footer.php");?>
 
