d7a2e47a2a9ac4ba746f35d1944206c948f853cd
Andrey Fits <andreyfits@gmail.com>
2020-05-02 13:27:28 +0300

    Calling the upload function

--- processing.php
+++ processing.php
@@ -1,5 +1,7 @@
 <?php
 require_once("includes/header.php");
+require_once("includes/classes/VideoUploadData.php");
+require_once("includes/classes/VideoProcessor.php");
 
 if (!isset($_POST["uploadButton"])) {
 	echo "No file sent to page.";
@@ -9,6 +11,8 @@ if (!isset($_POST["uploadButton"])) {
 // 1) Create file upload data
 $videoUploadData = new VideoUploadData($_POST["fileInput"], $_POST["titleInput"], $_POST["descriptionInput"], $_POST["privacyInput"], $_POST["categoriesInput"], "REPLACE-THIS");
 // 2) Process video data (upload)
+$videoProcessor = new VideoProcessor($con);
+$wasSuccessful = $videoProcessor->upload($videoUploadData);
 
 // 3) Check if upload was successful
 ?>
