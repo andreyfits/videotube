a3d9de504ac251738c99502b3240bfb1853c613c
Andrey Fits <andreyfits@gmail.com>
2020-05-02 22:12:13 +0300

    Generating the thumbnail images

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -73,6 +73,8 @@ class VideoProcessor
 				echo "Upload failed - could not generate thumbnails\n";
 				return false;
 			}
+
+			return true;
 		}
 	}
 
@@ -164,6 +166,40 @@ class VideoProcessor
 		$videoId = $this->con->lastInsertId();
 		$this->updateDuration($duration, $videoId);
 
+		for ($num = 1; $num <= $numThumbnails; $num++) {
+			$imageName = uniqid() . ".jpg";
+			$interval = ($duration * 0.8) / $numThumbnails * $num;
+			$fullThumbnailPath = "$pathToThumbnail/$videoId-$imageName";
+
+			$cmd = "$this->ffmpegPath -i $filePath -ss $interval -s $thumbnailSize -vframes 1 $fullThumbnailPath 2>&1";
+
+			$outputLog = array();
+			exec($cmd, $outputLog, $returnCode);
+
+			if ($returnCode != 0) {
+				//Command failed
+				foreach ($outputLog as $line) {
+					echo $line . "<br>";
+				}
+			}
+
+			$query = $this->con->prepare("INSERT INTO thumbnails(videoId, filePath, selected)
+                                        	VALUES(:videoId, :filePath, :selected)");
+			$query->bindParam(":videoId", $videoId);
+			$query->bindParam(":filePath", $fullThumbnailPath);
+			$query->bindParam(":selected", $selected);
+
+			$selected = $num == 1 ? 1 : 0;
+
+			$success = $query->execute();
+
+			if (!$success) {
+				echo "Error inserting thumbnail\n";
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	private function getVideoDuration($filePath)
