28ee2c65d6d26fe86553b3c44047febb5a874ee2
Andrey Fits <andreyfits@gmail.com>
2020-05-02 22:25:54 +0300

    Showing a loading spinner on upload

--- processing.php
+++ processing.php
@@ -16,4 +16,7 @@ $videoProcessor = new VideoProcessor($con);
 $wasSuccessful = $videoProcessor->upload($videoUploadData);
 
 // 3) Check if upload was successful
+if ($wasSuccessful) {
+	echo "Upload successful";
+}
 ?>
--- upload.php
+++ upload.php
@@ -11,4 +11,23 @@ require_once("includes/classes/VideoDetailsFormProvider.php");
 		?>
     </div>
 
+    <script>
+        $("form").submit(function () {
+            $("#loadingModal").modal("show");
+        });
+    </script>
+
+    <!-- Modal -->
+    <div class="modal fade" id="loadingModal" tabindex="-1" role="dialog" aria-labelledby="loadingModal" aria-hidden="true" data-backdrop="static"
+         data-keyboard="false">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    Please wait. This might take a while.
+                    <img src="assets/images/icons/loading-spinner.gif" alt="Spinner">
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php require_once("includes/footer.php"); ?>
