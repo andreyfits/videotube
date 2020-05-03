e96123258c5420492bdad6cc11e169d1e7ef0814
Andrey Fits <andreyfits@gmail.com>
2020-05-03 19:51:50 +0300

    Starting our validation functions

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -4,6 +4,7 @@
 class Account
 {
 	private $con;
+	private $errorArray = array();
 
 	public function __construct($con)
 	{
@@ -12,6 +13,13 @@ class Account
 
 	public function register($fn, $ln, $un, $em, $em2, $pw, $pw2)
 	{
+		$this->validateFirstName($fn);
+	}
 
+	private function validateFirstName($fn)
+	{
+		if (strlen($fn) > 25 || strlen($fn) < 2) {
+			array_push($this->errorArray, Constants::$firstNameCharacters);
+		}
 	}
 }
