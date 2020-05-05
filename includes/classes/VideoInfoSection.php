aaa74ea05519572e5a889b3b36e00273cc419673
Andrey Fits <andreyfits@gmail.com>
2020-05-05 10:27:49 +0300

    Creating the video info controls class

--- includes/classes/VideoInfoSection.php
+++ includes/classes/VideoInfoSection.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once("includes/classes/VideoInfoControls.php");
 
 class VideoInfoSection
 {
@@ -22,10 +22,14 @@ class VideoInfoSection
 		$title = $this->video->getTitle();
 		$views = $this->video->getViews();
 
+		$videoInfoControls = new VideoInfoControls($this->video, $this->userLoggedInObj);
+		$controls = $videoInfoControls->create();
+
 		return "<div class='videoInfo'>
                     <h1>$title</h1>
                     <div class='bottomSection'>
                         <span class='viewCount'>$views views</span>
+                        $controls
                     </div>
                 </div>";
 	}
