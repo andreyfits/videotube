93f491e2a959fb3142aca9f7fb5467af845fad4e
Andrey Fits <andreyfits@gmail.com>
2020-05-06 20:29:13 +0300

    Indenting the comment replies

--- assets/js/commentActions.js
+++ assets/js/commentActions.js
@@ -8,7 +8,13 @@ function postComment(button, postedBy, videoId, replyTo, containerClass) {
             commentText: commentText, postedBy: postedBy,
             videoId: videoId, responseTo: replyTo
         }).done(function (comment) {
-            $("." + containerClass).prepend(comment);
+
+            if (!replyTo) {
+                $("." + containerClass).prepend(comment);
+            } else {
+                $(button).parent().siblings("." + containerClass).append(comment);
+            }
+
         });
     } else {
         alert("You can't post an empty comment");
