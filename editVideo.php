3246660887724ac67c31510f63fcfb060b4ff239
Andrey Fits <andreyfits@gmail.com>
2020-05-09 20:35:02 +0300

    Getting the video thumbnails

--- editVideo.php
+++ editVideo.php
@@ -3,6 +3,7 @@ require_once("includes/header.php");
 require_once("includes/classes/VideoPlayer.php");
 require_once("includes/classes/VideoDetailsFormProvider.php");
 require_once("includes/classes/VideoUploadData.php");
+require_once("includes/classes/SelectThumbnail.php");
 
 if (!User::isLoggedIn()) {
 	header("Location: signIn.php");
@@ -18,3 +19,18 @@ if ($video->getUploadedBy() != $userLoggedInObj->getUsername()) {
 	echo "Not your video";
 	exit();
 }
+?>
+<div class="editVideoContainer column">
+    <div class="topSection">
+		<?php
+		$videoPlayer = new VideoPlayer($video);
+		echo $videoPlayer->create(false);
+
+		$selectThumbnail = new SelectThumbnail($con, $video);
+		echo $selectThumbnail->create();
+		?>
+    </div>
+    <div class="bottomSection">
+
+    </div>
+</div>
