3b3a798fa76735e2c2d6e53a655408522b1ab173
Andrey Fits <andreyfits@gmail.com>
2020-05-11 19:24:17 +0300

    Edit video form handler

--- editVideo.php
+++ editVideo.php
@@ -19,6 +19,29 @@ if ($video->getUploadedBy() != $userLoggedInObj->getUsername()) {
 	echo "Not your video";
 	exit();
 }
+
+$detailsMessage = "";
+if (isset($_POST["saveButton"])) {
+	$videoData = new VideoUploadData(
+		null,
+		$_POST["titleInput"],
+		$_POST["descriptionInput"],
+		$_POST["privacyInput"],
+		$_POST["categoryInput"],
+		$userLoggedInObj->getUsername()
+	);
+
+	if ($videoData->updateDetails($con, $videoId)) {
+		$detailsMessage = "<div class='alert alert-success'>
+                               <strong>SUCCESS!</strong> Details updated successfully!
+                           </div>";
+		$video = new Video($con, $_GET["videoId"], $userLoggedInObj);
+	} else {
+		$detailsMessage = "<div class='alert alert-danger'>
+                               <strong>ERROR!</strong> Something went wrong
+                           </div>";
+	}
+}
 ?>
 <script src="assets/js/editVideoActions.js"></script>
 <div class="editVideoContainer column">
